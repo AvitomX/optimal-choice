0a475f37ef34f5ebdb1195bf1b442bdf8d0f05ad
Aleksandr <aleksandr0409@gmail.com>
2023-04-01 21:51:09 +0300

    api + app

---
+++ optimal-choice-app/src/main/java/ru/papapers/optimalchoice/OptimalChoiceApplication.java
@@ -0,0 +1,13 @@
+package ru.papapers.optimalchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimalChoiceApplication.class, args);
+	}
+
+}
